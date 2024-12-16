844d4fcc664e8b9831989d5a5a854b0ecfb1806a
Mikah <emperormikah@gmail.com>
2024-12-16 16:15:01 +0200

    Add files via upload
    this python file check if you can divide a number by 3

---
+++ divide-x-by-3.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+def is_divisible_by_3(x):
+    """Check if 3 can divide x without a remainder."""
+    if x % 3 == 0:
+        return True
+    return False
+
+# Input from the user
+try:
+    x = int(input("Enter a number: "))
+    if is_divisible_by_3(x):
+        print(f"Yes, 3 can divide {x}.")
+    else:
+        print(f"No, 3 cannot divide {x}.")
+except ValueError:
+    print("Please enter a valid integer.")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
