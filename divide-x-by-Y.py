197707bafbc8a91a0fad84f705dea06ae7571d7f
Mikah <emperormikah@gmail.com>
2025-07-29 13:59:31 +0300

    Update divide-x-by-Y.py

--- divide-x-by-Y.py
+++ divide-x-by-Y.py
@@ -1,16 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 def is_divisible_by_y(x):
     """Check if 3 can divide x without a remainder."""
     if x % y == 0:
         return True
     return False
 
-# Input from the user
 try:
     x = int(input("Enter a number: "))
     y = int(input("Enter a divider: "))
