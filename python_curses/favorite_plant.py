4682ec10bf9d8817ad70007867d2912c85ef8d21
Mikah <emperormikah@gmail.com>
2025-08-29 13:03:34 +0300

    Create favorite_plant.py

---
+++ python_curses/favorite_plant.py
@@ -0,0 +1,13 @@
+plant = str(input("Write Something:"))
+
+if plant == "spathiphyllum":
+    print("No, I want a big Spathiphyllum!")
+
+elif plant == "pelargonium":
+    print("Spathiphyllum! Not pelargonium!")
+
+elif plant == "Spathiphyllum":
+    print("Yes - Spathiphyllum is the best plant ever!")
+
+else:
+    print(plant)
